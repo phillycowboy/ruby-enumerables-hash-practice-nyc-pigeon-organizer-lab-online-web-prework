ea86ef1ae26ca99a6cb0154e1c086baa4e4ee610
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-28 16:50:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,21 @@
 
-def nyc_pigeon_organizer(data)
+# def nyc_pigeon_organizer(data)
   # write your code here!
+  # final_results=data.each_with_object({}) do |(key, value), final_array|
+  #   value.each do |inner_key, names|
+  #     names.each do |name|
+  #       if !final_array[name]
+  #         final_array[name]={}
+  #       end
+  #       if !final_array[name][key]
+  #         !final_array [name][key]=[]
+  #       end
+  #       final_array[name][key].push(inner_key.to_s)
+  #     end
+  #   end
+  # end
+  require 'pry'
+def nyc_pigeon_organizer(data)
   final_results=data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
@@ -8,11 +23,10 @@ def nyc_pigeon_organizer(data)
           final_array[name]={}
         end
         if !final_array[name][key]
-          !final_array [name][key]=[]
+          !final_array[name][key]=[]
         end
         final_array[name][key].push(inner_key.to_s)
       end
     end
-  end
-
+end
 end
